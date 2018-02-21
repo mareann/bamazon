ceb6add1b2bc613d3f11c2b8603a49850e2f0c81
Maryann Jordan <majch@DESKTOP-KQJEU0U.(none)>
2018-02-20 22:23:54 -0600

    add update inventory

---
+++ bamazon.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  item_id INTEGER(10) NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(15),
+  price DECIMAL(10,2) NULL,
+  stock_quantity INTEGER NULL,
+  PRIMARY KEY (item_id)
+);
+	ALTER TABLE products AUTO_INCREMENT = 100;
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Harry Potter And The Order Of The Phoenix   ", "Books",8.50, 10),
+       ("Harry Potter And The Goblet Of Fire         ", "Books",9.50, 15),
+       ("End of Watch: A Novel                       ", "Books",16.50, 3),
+       ("Justice League with Henry Cavill, Amy Adams ","MOVIES",24.95,20),
+       ("Wonder with Julia Roberts                   ","MOVIES",18.95,25),
+       ("The Greatest Showman                        ","MOVIES",19.95,18),
+       ("DCU: Batman: Gotham By Gaslight             ","MOVIES",16.95,48),
+       ("By The Way, I Forgive You Brandi Carlile    ","Music",10.75,77),
+       ("Man of the Woods Justin Timberlake          ","Music",9.95,43),
+       ("Rumours Fleetwood Mac                       ","Music",20.95,26)
+-- ### Alternative way to insert more than one row
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql")
 var inquirer = require("inquirer")
 
-
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
+  connectionLimit : 10,
   host: "localhost",
   port: 3306,
 
@@ -14,146 +14,113 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
+/* 
+pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});*/
 
-connection.connect(function(err) {
+pool.getConnection( function(err,connection) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  afterConnection();
+  poolConnection = connection;
+  afterConnection(connection);
 });
 
 var inventory = {}
 var inventoryNumberDisplayed = 0;
 var choicesAvailable = []
+var itemID = 0;
+//var itemIDtmp = '';
+var finalCost = 0;
+var orderQuantity = 0;
+
 function userChoices()
 {
    for (var j=0;j<inventoryNumberDisplayed;j++)
 	{
-   	choicesAvailable[j]=String(inventory[j].product)+" "+"$"+String(inventory[j].price)//+" "inventory[j].product.toString()
-   	//+" "+inventory[j].price.toString
+   	choicesAvailable[j]=String(inventory[j].product)+" price:$"+String(inventory[j].price)+String(" id: "+inventory[j].id
+   	+":"+inventory[j].stock_quantity)
    }
-  // return(choicesAvailable)
+ 
+}
+function updateQuantity(connection,item,quantity) {
+  //console.log("update connected as id " + connection.threadId);
+  
+  var query2 = "UPDATE products SET stock_quantity=stock_quantity-"+quantity+" WHERE item_id="+item
+  
+  connection.query(query2, function(err,result) {
+  	if (err) throw err;
+  	//console.log(result.affectedRows + " record(s) updated");
+  	if (result.affectedRows)
+  	{
+  	  console.log("Order processed successfully!")
+  	  console.log("Total Cost: "+"$"+finalCost+" Hope to see you soon!")
+  	}
+  	process.exit(0)
+    
+  });
 }
 
-function afterConnection() {
-  connection.query("SELECT item_id as id,product_name as product,price FROM products", 
-  	function(err, result) { //, fields) {
+function afterConnection(connection) {
+	var query1 = "SELECT item_id as id,product_name as product,price,stock_quantity FROM products"
+    connection.query(query1, function(err, result) { //, fields) {
     if (err) throw err;
-   // console.log(result);
+ 
     console.log("  BAMAZON BEST SELLERS")
     inventory = result;
     inventoryNumberDisplayed = result.length;
-
-    for (var i=0;i<result.length;i++)
-    {
-      
-      console.log("  "+result[i].id+" "+result[i].product+" "+result[i].price);
-    }
     userChoices()
-// Create a "Prompt" with a series of questions.
-inquirer
-  .prompt([
-    // Here we create a basic text prompt.
-    {
+    // Create a "Prompt" with a series of questions.
+    inquirer
+    .prompt([
+      // Here we create a basic text prompt.
+      {
       //name: availableProducts,
-      //type: "input",
-      type: 'rawlist',
-      message: "  Please enter ID of product you would like to buy",
-      name: "itemID",
-      choices: choicesAvailable
-  //            [ "1 Harry Potter And The Order Of The Phoenix 8.5",
-  //"2 Harry Potter And The Goblet Of Fire 9.5",
-  //"3 End of Watch: A Novel (The Bill Hodges Trilogy) 16.5"             ]
-      //userChoices()   //["one","two"]
-      //validate: validateName
+        type: 'rawlist',
+        message: "  Please enter number of product you would like to buy",
+        name: "itemDesc",
+        choices:  choicesAvailable
+
+  // [ "1 Harry Potter And The Order Of The Phoenix 8.5",
+  //   "2 Harry Potter And The Goblet Of Fire 9.5",
+  //   "3 End of Watch: A Novel (The Bill Hodges Trilogy) 16.5"             ]
     },
     {
     message: "  How many would you like to buy?",
     type: "input",
-    name: "quantityID",
-    //validate: validateName
+    name: "quantityNum"
+    
     }
   ])
   .then(function(inquirerResponse) {
 
-      console.log("\n"+inquirerResponse.itemID+" "+inquirerResponse.quantityID);
-      //console.log(inventory[inquirerResponse.itemID-1].id)  
+      //console.log("\n"+inquirerResponse.itemDesc)
+      //console.log(inquirerResponse) 
+      var tmp = inquirerResponse.itemDesc.split(":")
+      itemID = parseInt(tmp[tmp.length-2])
+      var tmp2 = tmp[tmp.length-3].split(" ") 
+      var Price = parseFloat(tmp2[0].replace('$',''))
+      //console.log("selected item "+itemID);
+      var inventoryNum = parseInt(tmp[tmp.length-1])
+      //console.log(" inventory of "+inventoryNum)
+      orderQuantity = parseInt(inquirerResponse.quantityNum);
+      if ( inquirerResponse.quantityNum <= inventoryNum)
+      {
+      	console.log("YAY! Those are available!")
+      	
+      	finalCost = Price*orderQuantity;
+      	
+      	updateQuantity(connection,itemID,inquirerResponse.quantityNum)
+      }
+      else
+	  {
+      	console.log("Sorry. Please reduce your quantity.")
+      	console.log("need new prompt here")
+      	process.exit(0)
+	  }
    });
-
-    connection.end();
+   
   }); // function err,result
-} // afterConnection
-/*
-function fixRes(res) {
-	var newRes = ""
-	var resRowCt = res.length
-	for (var i=0;i<res.length;i++)
-	{
-		console.log("res[+"+i+"len "+res[i].length)
-        //for (var j=0;j<res[i].length;j++)
-        //  {
-          	//take out [{,}]
-          	res[i] = res[i].replace('[','')
-          	res[i] = res[i].replace(']','')
-          	res[i] = res[i].replace('{','')
-          	res[i] = res[i].replace('}','')
-          	console.log("i "+i+" "+res[i])
-         // }
 
-	}
-} // end fixRes
-*/
-/*
-// Create a "Prompt" with a series of questions.
-inquirer
-  .prompt([
-    // Here we create a basic text prompt.
-    {
-      type: "input",
-      message: "Please enter ID of product you would like to buy",
-      name: "itemID",
-      //validate: validateName
-    },
-    {
-    message: "How many would you like to buy?",
-    type: "input",
-    name: "quantityID",
-    //validate: validateName
-    },
-    // Here we create a basic password-protected text prompt.
-   // {
-   //   type: "password",
-   //   message: "Set your password",
-   //   name: "password"
-   // },
-   // {
-   //   type: "confirm",
-   //   message: "Are you sure:",
-   //   name: "confirm",
-   //   default: true
-   // },
-    // Here we give the user a list to choose from.
-   // {
-   //   type: "rawlist",
-   //   message: "Which do you prefer?",
-   //   choices: ["Mr", "Mrs", "Ms"],
-   //   name: "hithere"
-    }
-    //,
-    // Here we ask the user to confirm.
-   // {
-   //   type: "confirm",
-   //   message: "You prefer that?",
-   //   name: "confirm",
-   //   default: true
-   // }
-  ])
-  .then(function(inquirerResponse) {
-    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
-    if (inquirerResponse.confirm) {
-      console.log("\nWelcome " + inquirerResponse.hithere+" "+inquirerResponse.username);
-      //console.log("Your " + inquirerResponse.pokemon + " is ready for battle!\n");
-    }
-    else {
-      console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
-    }
-  });*/
+} // afterConnection
